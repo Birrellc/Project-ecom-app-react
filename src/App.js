79ad62c5f9aff5a6a9d76a16bcbaca5eb8c7f352
Chris <65775571+Birrellc@users.noreply.github.com>
2022-07-21 18:30:11 +0100

    Chore: setup skeleton for app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='categories-container'>
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+          <h2>Hats</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+          <h2>Jackets</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+          <h2>Shoes</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+          <h2>Women's</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* img */}
+        <div className='category-body-container'>
+          <h2>Men's</h2>
+          <h2>Shop Now</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
