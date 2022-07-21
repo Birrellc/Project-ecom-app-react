cd35f4893360edb57ece1e2d5a770b7cfa451601
Chris <65775571+Birrellc@users.noreply.github.com>
2022-07-21 18:39:31 +0100

    Chore: cleaned up reusable code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
 import React from 'react';
 
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Shoes',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
+
   return (
     <div className='categories-container'>
-      <div className='category-container'>
-        {/* img */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <h2>Shop Now</h2>
+      {/* map the object array onto the page (title = array destructuring) */}
+
+      {categories.map(({ title }) => (
+        <div className='category-container'>
+          {/* img */}
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <h2>Shop Now</h2>
+          </div>
         </div>
-      </div>
-      <div className='category-container'>
-        {/* img */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <h2>Shop Now</h2>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* img */}
-        <div className='category-body-container'>
-          <h2>Shoes</h2>
-          <h2>Shop Now</h2>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* img */}
-        <div className='category-body-container'>
-          <h2>Women's</h2>
-          <h2>Shop Now</h2>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* img */}
-        <div className='category-body-container'>
-          <h2>Men's</h2>
-          <h2>Shop Now</h2>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
