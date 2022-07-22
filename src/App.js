5a119c773c7d100d1a5d9856544da2b422d636f8
Chris <65775571+Birrellc@users.noreply.github.com>
2022-07-22 19:27:06 +0100

    Feat: Added navigation to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Navigation from './pages/Navigation/Navigation';
 import Home from './pages/Home/Home';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
