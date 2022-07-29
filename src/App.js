8a6eaf9e0fe1ebce1e939cc3d46a80f60c72af74
Chris <65775571+Birrellc@users.noreply.github.com>
2022-07-29 13:26:14 +0100

    Feat: added base of shop page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from './pages/Navigation/Navigation';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import Shop from './pages/Shop/Shop';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
         <Route path='login' element={<Login />} />
       </Route>
     </Routes>
