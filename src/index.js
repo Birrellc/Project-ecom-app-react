d085840fca4ff3ebf890df1ef33fb3329221bdaf
Birrellc <vaiezmedia@gmail.com>
2022-09-05 13:26:54 +0100

    Feat: created Cart Context

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/User';
 import { ProductsProvider } from './contexts/Products';
+import { CartProvider } from './contexts/Cart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,7 +13,9 @@ root.render(
   <BrowserRouter>
     <UserProvider>
       <ProductsProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </ProductsProvider>
     </UserProvider>
   </BrowserRouter>
