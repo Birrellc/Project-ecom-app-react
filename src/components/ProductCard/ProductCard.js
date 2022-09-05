af65ba9921110b476d71e151df80221be5fd56d8
Birrellc <vaiezmedia@gmail.com>
2022-09-05 11:39:50 +0100

    Feat: passed product data and productCard to shop.js

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,16 +1,17 @@
 import './ProductCard.scss';
 
-import Button from '../Button';
+import Button from '../Button/Button';
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
   return (
     <div className='product-card-container'>
-      <img src='' alt='' />
+      <img src={imageUrl} alt={`${name}`} />
       <div className='footer'>
-        <span className='name'></span>
-        <span className='price'></span>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
       </div>
-      <Button></Button>
+      <Button buttonType='inverted'>Add to card</Button>
     </div>
   );
 };
