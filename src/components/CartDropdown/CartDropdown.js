d313c0f0ad7e022d9a2c9a5d6e92ee9fd8bea42e
Birrellc <vaiezmedia@gmail.com>
2022-09-08 11:38:29 +0100

    Feat: added base cart items to the cart drop down

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/Cart';
+
 import Button from '../Button/Button';
+import CartItem from '../CartItem/CartItem';
 
 import './CartDropdown.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items'></div>
+      <div className='cart-items'>
+        {/* need to map the data onto the page */}
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>Go To Checkout</Button>
     </div>
   );
