5253840694541fa1f982aed03b227c184a8ff929
Birrellc <vaiezmedia@gmail.com>
2022-09-09 17:35:50 +0100

    Feat: added increment quantity feature to checkout

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,11 +1,26 @@
-import React from 'react';
-
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/Cart';
 import './Checkout.scss';
 
 const Checkout = () => {
+  const { cartItems, addItemToCart } = useContext(CartContext);
+
   return (
     <div>
       <h1>Checkout Page</h1>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>-</span>
+              <span>{quantity}</span>
+              <span onClick={() => addItemToCart(cartItem)}>+</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
